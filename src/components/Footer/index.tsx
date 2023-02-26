cca36e91fe1e6fa39bffb025d2c098c20be0f096
carlo-coding <jaanc.rt@gmail.com>
2023-02-26 17:55:37 -0600

    fixed build

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,37 +1,45 @@
 import { Box } from "@mui/material";
 import { styles_footer } from "./styled";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import MailIcon from '@mui/icons-material/Mail';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MailIcon from "@mui/icons-material/Mail";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
-export default function Footer(){
-
-    return (
-        <Box
-            sx={styles_footer}
-            component="footer"
-        >
-            <a href="https://github.com/carlo-coding" target="_blank">
-            <div className="contact-icon">
-                <GitHubIcon />
-            </div>
-            carlo-coding
-            </a>
-            <a href="mailto:jaanc.rt@gmail.com" target="_blank">
-            <div className="contact-icon">
-                <MailIcon />
-            </div>
-                jaanc.rt@gmail.com
-            </a>
-            <a href="https://www.linkedin.com/in/jaan-carlo/" target="_blank">
-                <LinkedInIcon />
-                jaan-carlo
-            </a>
-            <a href="https://twitter.com/janrt03243200" target="_blank">
-                <TwitterIcon /> 
-                @janrt03243200
-            </a>
-        </Box>
-    )
+export default function Footer() {
+  return (
+    <Box sx={styles_footer} component="footer">
+      <a
+        href="https://github.com/carlo-coding"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="contact-icon">
+          <GitHubIcon />
+        </div>
+        carlo-coding
+      </a>
+      <a href="mailto:jaanc.rt@gmail.com" target="_blank" rel="noreferrer">
+        <div className="contact-icon">
+          <MailIcon />
+        </div>
+        jaanc.rt@gmail.com
+      </a>
+      <a
+        href="https://www.linkedin.com/in/jaan-carlo/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedInIcon />
+        jaan-carlo
+      </a>
+      <a
+        href="https://twitter.com/janrt03243200"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <TwitterIcon />
+        @janrt03243200
+      </a>
+    </Box>
+  );
 }
--- src/components/IntsObserver/index.tsx
+++ src/components/IntsObserver/index.tsx
@@ -1,55 +1,55 @@
 import React, { useEffect, useRef, useState } from "react";
 
 interface ObserverListProps {
-    children(params: any): React.ReactNode,
-    toggleClass: string,
-    [x: string]: any
+  children(params: any): React.ReactNode;
+  toggleClass: string;
+  [x: string]: any;
 }
 
-export function ObserverList({ children, toggleClass, ...props }: ObserverListProps) {
-    const observer = useRef<IntersectionObserver|null>(null)
-    const [items, setItems] = useState<Array<HTMLLIElement>>([]);
+export function ObserverList({
+  children,
+  toggleClass,
+  ...props
+}: ObserverListProps) {
+  const observer = useRef<IntersectionObserver | null>(null);
+  const [items, setItems] = useState<Array<HTMLLIElement>>([]);
 
-    const saveItem = (item: HTMLLIElement)=> {
-        setItems((prevItems: Array<HTMLLIElement>) => [...prevItems, item])
-    }
+  const saveItem = (item: HTMLLIElement) => {
+    setItems((prevItems: Array<HTMLLIElement>) => [...prevItems, item]);
+  };
 
-    useEffect(()=> {
-        observer.current = new window.IntersectionObserver(entries=> {
-            entries.forEach(entry=> {
-                entry.target.classList.toggle(toggleClass, entry.isIntersecting)
-            })
-        });
-    }, [])
+  useEffect(() => {
+    observer.current = new window.IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        entry.target.classList.toggle(toggleClass, entry.isIntersecting);
+      });
+    });
+  }, [toggleClass]);
 
-    useEffect(()=> {
-        items.forEach(item=> observer.current?.observe(item))
-    }, [items]);
+  useEffect(() => {
+    items.forEach((item) => observer.current?.observe(item));
+  }, [items]);
 
-    return (
-        <ul {...props}>
-            {children({ saveItem })}
-        </ul>
-    )
+  return <ul {...props}>{children({ saveItem })}</ul>;
 }
 
-interface ObservedItem {
-    saveItem(param: HTMLElement): void, 
-    children: React.ReactNode,
-    [x: string]: any
+interface IObservedItem {
+  saveItem(param: HTMLElement): void;
+  children: React.ReactNode;
+  [x: string]: any;
 }
 
-export function ObservedItem({ saveItem, children, ...props }: ObservedItem) {
-    const liRef = useRef<any>(null);
+export function ObservedItem({ saveItem, children, ...props }: IObservedItem) {
+  const liRef = useRef<any>(null);
 
-    useEffect(()=>{
-        if (!liRef.current) return;
-        saveItem(liRef.current as HTMLLinkElement);
-    }, []);
+  useEffect(() => {
+    if (!liRef.current) return;
+    saveItem(liRef.current as HTMLLinkElement);
+  }, [saveItem]);
 
-    return (
-        <li ref={liRef} {...props}>
-            { children }
-        </li>
-    )
+  return (
+    <li ref={liRef} {...props}>
+      {children}
+    </li>
+  );
 }
